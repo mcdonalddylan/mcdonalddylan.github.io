bfe7ae712fab92f845443ca1e079fd9228ec6498
mcdonalddylan <soupaface@gmail.com>
2023-08-29 20:01:29 -0500

    Added modal functionality + Smaller refinements

--- js/main-page-utils.js
+++ js/main-page-utils.js
@@ -8,8 +8,6 @@
     }
 
     function dertmineSuperscriptAbbreviation (birthdayValue) {
-        console.log(birthdayValue);
-        console.log(birthdayValue % 10);
         if (birthdayValue % 10 === 1) {
             return 'st';
         }
@@ -24,6 +22,27 @@
         }
     }
 
+    const modal = document.querySelector('.modal-bg');
+    function toggleModal () {
+        const formBody = document.querySelector('.form-body');
+        if (modal && modal.style.display === 'none') {
+            modal.style.display = 'block';
+            formBody.classList.add('blur-body');
+        } else {
+            modal.style.display = 'none';
+            formBody.classList.remove('blur-body');
+        }
+    }
+
+    function submitForm (event) {
+        event.preventDefault();
+    
+        const favoriteMemoryComment = event.currentTarget['fav-mem'].value;
+        console.log('favorite memory comment: ', favoriteMemoryComment);
+    
+        //TODO: do something with the favorite memory comment
+    }
+
     const birthdayNumber = document.querySelector('.birthday-number');
     birthdayNumberValue = calculateBirthdayNumber(BIRTHDAY_DATE);
     birthdayNumber.textContent = birthdayNumberValue;
@@ -31,18 +50,17 @@
     const birthdayNumberSuperscript = document.querySelector('.birthday-number-superscript');
     birthdayNumberSuperscript.textContent = dertmineSuperscriptAbbreviation(birthdayNumberValue);
 
+    const form = document.querySelector('.form');
+    form.addEventListener('submit', submitForm);
+
     const hiddenButton = document.querySelector('.hidden-button');
-    hiddenButton.addEventListener('click', function () {
-        //TODO: show modal with loving message :D
-        alert('Love youuuuu <3');
-    });
+    const randomHeight = (Math.floor(Math.random() * window.screen.height)-150).toString().concat('px');
+    const randomWidth = (Math.floor(Math.random() * window.screen.width)-200).toString().concat('px');
+    hiddenButton.style.top = randomHeight;
+    hiddenButton.style.left = randomWidth;
+    hiddenButton.addEventListener('click', toggleModal);
+    modal.addEventListener('click', toggleModal);
+
+    const closeModalButton = document.querySelector('.modal-close-button');
+    closeModalButton.addEventListener('click', toggleModal);
 })();
-
-function submitForm (event) {
-    event.preventDefault();
-
-    const favoriteMemoryComment = event.currentTarget['fav-mem'].value;
-    console.log('favorite memory comment: ', favoriteMemoryComment);
-
-    //TODO: do something with the favorite memory comment
-}
